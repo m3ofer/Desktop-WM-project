3deba244030f450f3a7556dc60f0d80535ca8589
hamza barbara <62608203+m3ofer@users.noreply.github.com>
2022-06-17 15:42:00 -0700

    Delete bin directory

--- bin/antyclick.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-a="$(bspc wm --get-status | grep -oE [1-9] | xargs)"
-case $a in
-  "1 1 1 2 3 4 5")echo "$(bspc monitor -d 1 2 3 4)";;
-  "1 1 1 2 3 4")echo "$(bspc monitor -d 1 2 3)";;
-  "1 1 1 2 3")echo "$(bspc monitor -d 1 2)";;
-  "1 1 1 2")echo "$(bspc monitor -d 1)";;
-esac
--- bin/dark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp ~/.config/theme/d/bspwmrc ~/.config/bspwm/bspwmrc &
-cp ~/.config/theme/d/gtk.css ~/.config/gtk-3.0/gtk.css &
-xfce4-panel-profiles load ~/.config/theme/d/dark.tar.bz2 &
-bspc wm -r 
-xwinwrap -fs -fdt -ni -b -nf -un -debug -- mpv -wid WID --loop --no-audio ~/Documents/Media/Videos/livewallpaper/zed.mp4
